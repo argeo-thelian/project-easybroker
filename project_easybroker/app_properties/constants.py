d5b9ff6ac8b690c6a331ca31340d48c45fb71308
argeo-thelian <argeo.thelian@gmail.com>
2022-08-09 09:42:47 -0500

    rm keys

--- project_easybroker/app_properties/constants.py
+++ project_easybroker/app_properties/constants.py
@@ -1,8 +1,10 @@
 from urllib.parse import urljoin
+from decouple import config
 
+HEADER_KEY_EB = config('HEADER_KEY_EB')
 BASE_URL = 'https://api.stagingeb.com/v1/'
 
 BASE_URL_PROPERTIES = urljoin(BASE_URL,'properties')
 BASE_URL_CONTACT = urljoin(BASE_URL,"contact_requests/")
-HEADERS = {'X-Authorization': 'l7u502p8v46ba3ppgvj5y2aad50lb9'}
+HEADERS = {'X-Authorization': HEADER_KEY_EB}
 HEADERS_CONTENT = {**HEADERS,'Content-type': 'application/json'} 
